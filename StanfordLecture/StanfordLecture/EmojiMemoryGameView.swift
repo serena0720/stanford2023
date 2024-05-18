c31ea44af345e6e6b2d38a889ff0dc7948cb34ac
Serena <sha0720@naver.com>
2024-05-19 01:02:33 +0900

    lecture 6: ViewBuilder

--- StanfordLecture/StanfordLecture/EmojiMemoryGameView.swift
+++ StanfordLecture/StanfordLecture/EmojiMemoryGameView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+    
     var body: some View {
         VStack {
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            cards
+                .animation(.default, value: viewModel.cards)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -23,14 +23,17 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2/4, contentMode: .fill)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
+    private var cards: some View {
+        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) { card in
+            if card.id.last == "b" {
+                VStack {
+                    CardView(card)
+                        .aspectRatio(2/4, contentMode: .fill)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                    Text(card.id)
                 }
             }
         }
